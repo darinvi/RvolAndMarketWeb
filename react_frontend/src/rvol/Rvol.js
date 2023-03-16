2d4b5838e83ba9065cf6bf9cb79af49ef564d03c
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-03-16 22:03:58 +0200

    strugling with second button

--- react_frontend/src/rvol/Rvol.js
+++ react_frontend/src/rvol/Rvol.js
@@ -1,10 +1,13 @@
 import '../App.css';
 import GetRvol from './components/GetRvol.js';
-
+import RenderCharts from './components/RenderCharts';
 
 export default function Rvol() {
   document.title = 'RVOL calculator and stats'
   return (
-      <GetRvol />   
+      <>
+      <GetRvol />
+      <RenderCharts />
+      </>
   )
 }
---
+++ react_frontend/src/rvol/components/RenderCharts.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+export default function RenderCharts(){
+    return (
+        <div className='GetRvol'>
+        <h3>FDFDS</h3>
+        <button className='MyButton'>Load chart</button>   
+        </div>
+    )
+}
